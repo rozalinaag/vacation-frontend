7288e3775ac5164d67c3f21fa8dc70a2a36024f6
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2023-05-02 19:22:14 +0300

    added redux-toolkit and axios query

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {Provider} from "react-redux";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 import {BrowserRouter} from 'react-router-dom'
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -14,7 +16,9 @@ root.render (
   <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
   </div>
---
+++ src/redux/posts.ts
@@ -0,0 +1,44 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from '../axios'
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const {data} = await axios.get('/posts');
+    return data;
+});
+
+type Props = {
+    posts: { items: string[], status: string },
+    tags: { items: string[], status: string },
+}
+const initialState: Props = {
+    posts: {
+        items: [],
+        status: 'loading',
+    },
+    tags: {
+        items: [],
+        status: 'loading'
+    }
+}
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(fetchPosts.pending, (state) => {
+            state.posts.items = [];
+            state.posts.status = 'loading';
+        })
+        builder.addCase(fetchPosts.fulfilled, (state, action) => {
+            state.posts.items = action.payload;
+            state.posts.status = 'loaded';
+        })
+        builder.addCase(fetchPosts.rejected, (state) => {
+            state.posts.items = [];
+            state.posts.status = 'error';
+        })
+    }
+});
+
+export const postsReducer = postsSlice.reducer;
