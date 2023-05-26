54cc542fed40ebad9667cc7a77f4fb547f9428ad
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2023-05-26 16:01:38 +0300

    Added modal to logout, notification

--- src/components/Header/Notification/index.tsx
+++ src/components/Header/Notification/index.tsx
@@ -1,46 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BellOutlined} from "@ant-design/icons";
 // @ts-ignore
 import styles from "./styles.module.scss";
-import {Dropdown} from "antd";
+import {Dropdown, Button, Modal} from "antd";
 import type {MenuProps} from 'antd';
-import {Link} from 'react-router-dom'
 
 function Notification() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [message, setMessage] = useState('');
 
-    // @ts-ignore
-    const items: MenuProps['items'] = [
+    const items: any = [
         {
+            label: 'Заявление №421 согласовано',
             key: '1',
-            label: (
-                <Link to={'/'}>
-                    Заявление №421 согласовано
-                </Link>
-            ),
         },
         {
+            label: 'Заявление отправлено на согласование',
             key: '2',
-            label: (
-                <Link to={'/'}>
-                    Заявление №421 отправлено на согласование
-                </Link>
-            ),
         },
         {
+            label: 'Заявление создано',
             key: '3',
-            label: (
-                <Link to={'/'}>
-                    Заявление №421 создано
-                </Link>
-            ),
         },
     ];
 
+    const onClick: MenuProps['onClick'] = ({key}: { key: string }): void => {
+        setIsModalOpen(true);
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].key === key) {
+                setMessage(items[i].label)
+            }
+        }
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div className={styles.wrapper}>
-            <Dropdown menu={{items}} placement="bottom" arrow>
-                <BellOutlined className={styles.bell}/>
+            <Dropdown menu={{items, onClick}} placement="bottom" arrow>
+                <a onClick={(e) => e.preventDefault()}>
+                    <BellOutlined className={styles.bell}/>
+                </a>
             </Dropdown>
+            <Modal title="Уведомление:" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                {message}
+            </Modal>
         </div>
     );
 }
--- src/components/Header/UserHeader/index.tsx
+++ src/components/Header/UserHeader/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "@mui/material/Button";
 import {logout} from "../../../redux/slices/auth";
 import {useDispatch, useSelector} from "react-redux";
-import {Menu, Dropdown} from 'antd';
+import {Menu, Modal} from 'antd';
 import {Link} from "react-router-dom";
 import type {MenuProps} from 'antd';
 // @ts-ignore
 import styles from './styles.module.scss'
-import {UserOutlined, BellOutlined} from "@ant-design/icons";
+import {UserOutlined} from "@ant-design/icons";
 import Notification from "../Notification";
 
 function UserHeader() {
     const dispatch = useDispatch();
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
     const userData = useSelector((state: any) => state.auth.data)
 
     type MenuItem = Required<MenuProps>['items'][number];
@@ -32,19 +33,25 @@ function UserHeader() {
         } as MenuItem;
     }
 
-    const onClickLogout = () => {
-        if (window.confirm('Выйти из приложения?')) {
-            dispatch(logout())
-            //добавить модалку позже
-            window.localStorage.removeItem('token')
-        }
+    const handleOk = () => {
+        setIsModalOpen(false);
+        dispatch(logout())
+        window.localStorage.removeItem('token')
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    const showModal = () => {
+        setIsModalOpen(true);
     };
 
     const items: MenuProps['items'] = [
-        getItem(userData.fullName, 'sub1',  <UserOutlined />, [
-            getItem(<Link to = '/cabinet'>Личный кабинет</Link>, '1'),
-            getItem(<Link to = '/'>На главную</Link>, '2'),
-            getItem(<div onClick={onClickLogout}>Выйти</div>, '3'),
+        getItem(userData.fullName, 'sub1', <UserOutlined/>, [
+            getItem(<Link to='/cabinet'>Личный кабинет</Link>, '1'),
+            getItem(<Link to='/'>На главную</Link>, '2'),
+            getItem(<div onClick={showModal}>Выйти</div>, '3'),
         ]),
     ];
 
@@ -52,7 +59,8 @@ function UserHeader() {
         <div className={styles.UserField}>
             <Notification/>
             <Link to="/posts/create">
-                <Button className={styles.create} style={{background: '#5666bd', boxShadow: 'none'}} variant="contained">Создать заявление</Button>
+                <Button className={styles.create} style={{background: '#5666bd', boxShadow: 'none'}}
+                        variant="contained">Создать заявление</Button>
             </Link>
             <div className={styles.UserFieldHeader}>
                 <Menu
@@ -63,6 +71,8 @@ function UserHeader() {
                     triggerSubMenuAction={"click"}
                 />
             </div>
+            <Modal title="Выйти из приложения?" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            </Modal>
         </div>
     );
 }
